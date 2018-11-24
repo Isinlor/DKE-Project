8daccea25185b5da46d24d61d9d58a2fcd9bfb0b
SG <selimgilon@gmail.com>
2018-11-24 21:03:14 +0100

    Added game mode selection and graphs specification screens. Still work in progress.

--- sketchpad/selim/Frame1Viewer.java
+++ sketchpad/selim/Frame1Viewer.java
@@ -19,20 +19,20 @@ public class Frame1Viewer {
 
         frame.setLayout(null);
 
-        button1.setSize(FRAME_WIDTH/3,FRAME_HEIGHT-23);
-        button1.setLocation(0,0);
+        button1.setSize(FRAME_WIDTH/3,FRAME_HEIGHT-28);
+        button1.setLocation(0,4);
         frame.add(button1);
         ActionListener listener1 = new ClickListenerFrame1();
         button1.addActionListener(listener1);
 
-        button2.setSize(FRAME_WIDTH/3,FRAME_HEIGHT-23);
-        button2.setLocation(300,0);
+        button2.setSize(FRAME_WIDTH/3,FRAME_HEIGHT-28);
+        button2.setLocation(300,4);
         frame.add(button2);
         ActionListener listener2 = new ClickListenerFrame1();
         button2.addActionListener(listener2);
 
-        button3.setSize(FRAME_WIDTH/3,FRAME_HEIGHT-23);
-        button3.setLocation(600,0);
+        button3.setSize(FRAME_WIDTH/3,FRAME_HEIGHT-28);
+        button3.setLocation(600,4);
         frame.add(button3);
         ActionListener listener3 = new ClickListenerFrame1();
         button3.addActionListener(listener3);
