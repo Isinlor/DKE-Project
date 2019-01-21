5f291b8aad60e91f5f24666526c11254f4e2d6d1
Tomasz Darmetko <isinlor@gmail.com>
2019-01-21 23:21:16 +0100

    Add LowerBoundBacktracking

---
+++ src/LowerBoundBacktracking.java
@@ -0,0 +1,120 @@
+import java.util.*;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.TimeoutException;
+
+public class LowerBoundBacktracking {
+
+    private Graph graph;
+    private int[] vertexColor;
+    private int[] startColoring;
+    private int maxColors;
+    private int startingVertex;
+    private SortedMap<Integer, Integer> nextVertexMap;
+
+    private long startTime;
+
+    public LowerBoundBacktracking(Graph graph) {
+        this.graph = graph;
+        setupVertexColor();
+        setupNextVertexMap();
+    }
+
+    public void colorClique(Set<Integer> clique) {
+        setupVertexColor();
+        int color = 1;
+        for (int vertex: clique) {
+            vertexColor[vertex] = color;
+            startColoring[vertex] = color;
+            color++;
+        }
+    }
+
+    public void findUpperBound() {
+
+        if(graph.getUpperBound() == graph.getLowerBound()) return;
+
+        for (int colors = graph.getLowerBound(); colors < graph.getUpperBound(); colors++) {
+            try {
+                if (canBeColoredWith(colors)) {
+                    graph.addUpperBound(colors);
+                } else {
+                    graph.addLowerBound(colors);
+                }
+            } catch (TimeoutException e) {
+                // no op
+            }
+        }
+
+    }
+
+    private boolean canBeColoredWith(int maxColors) throws TimeoutException {
+        this.startTime = System.currentTimeMillis();
+        this.maxColors = maxColors;
+        this.vertexColor = startColoring.clone();
+        return tryToColor(startingVertex);
+    }
+
+    /**
+     * ExactAlgorithm algorithm
+     */
+    private boolean tryToColor(int vertex) throws TimeoutException {
+
+        if(System.currentTimeMillis() - startTime > 5000) {
+            throw new TimeoutException();
+        }
+
+        if(vertex == 0) return true;
+        if(vertexColor[vertex] > 0) return tryToColor(nextVertex(vertex));
+
+        for(int color = 1; color <= maxColors; color++) {
+
+            if(isAvailable(vertex, color)) {
+                vertexColor[vertex] = color;
+                if (tryToColor(nextVertex(vertex))) {
+                    vertexColor[vertex] = 0;
+                    return true;
+                }
+                vertexColor[vertex] = 0;
+            }
+
+        }
+
+        return false;
+    }
+
+    private int nextVertex(int currentVertex) {
+        return nextVertexMap.get(currentVertex);
+    }
+
+    private void setupVertexColor() {
+        vertexColor = new int[graph.getNumberOfVertices() + 1];
+        Arrays.fill(vertexColor, 0);
+        startColoring = new int[graph.getNumberOfVertices() + 1];
+        Arrays.fill(startColoring, 0);
+    }
+
+    private void setupNextVertexMap() {
+        nextVertexMap = new ConcurrentSkipListMap<>();
+        SortedSet<Integer> sortedVertices = graph.getSortedVertices();
+        Iterator<Integer> sortedVerticesIteratorFrom = sortedVertices.iterator();
+        Iterator<Integer> sortedVerticesIteratorTo = sortedVertices.iterator();
+
+        startingVertex = sortedVerticesIteratorTo.next();
+        while(sortedVerticesIteratorFrom.hasNext()) {
+            int from = sortedVerticesIteratorFrom.next();
+            int to = sortedVerticesIteratorTo.hasNext() ? sortedVerticesIteratorTo.next() : 0;
+            nextVertexMap.put(from, to);
+        }
+    }
+
+    /**
+     * Check if we can color vertex with a specific color
+     */
+    private boolean isAvailable(int vertexA, int color) {
+        for (int vertexB: graph.getNeighbours(vertexA)) {
+            if (vertexColor[vertexB] == color) return false;
+        }
+        return true;
+    }
+
+}
