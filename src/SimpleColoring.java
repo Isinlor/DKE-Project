449578c452e7329177a746d0e2fb82c48bb1429a
Tomasz Darmetko <isinlor@gmail.com>
2019-01-21 11:35:26 +0100

    Add SimpleColoring, a 2-Color algorithm working in O(V+E) time.

---
+++ src/SimpleColoring.java
@@ -0,0 +1,47 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class SimpleColoring {
+
+    public static void color(Graph graph) {
+
+        int vertex = graph.getVertices().iterator().next();
+
+        int[] vertexColor = new int[graph.getNumberOfVertices() + 1];
+        vertexColor[vertex] = 1;
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(vertex);
+
+        while(!queue.isEmpty()) {
+            vertex = queue.remove();
+            int color = (vertexColor[vertex] % 2) + 1;
+            for (int neighbor: graph.getNeighbours(vertex)) {
+                if(vertexColor[neighbor] != 0) continue;
+                vertexColor[neighbor] = color;
+                queue.add(neighbor);
+            }
+            if(queue.isEmpty()) {
+                for (vertex = 1; vertex <= graph.getNumberOfVertices(); vertex++) {
+                    if(vertexColor[vertex] == 0) {
+                        queue.add(vertex);
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (Edge edge: graph.getEdges()) {
+            if(vertexColor[edge.from] == vertexColor[edge.to]) {
+                graph.addLowerBound(3);
+                return;
+            }
+        }
+
+        graph.addChromaticNumber(2);
+
+    }
+
+}
